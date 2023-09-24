203f28ec591e60c4dcb38f06eaf793f2a63f291b
Karl Morris <karl.morris@temple.edu>
2023-09-24 15:03:29 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -0,0 +1,28 @@
+package edu.temple.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    // Declare view properties - the first one is done for you
+    lateinit var displayTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize with views defined in Layout - the first one is done for you
+        displayTextView = findViewById(R.id.displayTextView)
+
+        
+        findViewById<Button>(R.id.clickMeButton).setOnClickListener {
+            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+        }
+
+
+    }
+}
